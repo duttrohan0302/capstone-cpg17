d07a46a6e63c9dc96d0f6495aaee710c8cec8d40
duttrohan0302 <duttrohan0302@gmail.com>
2021-12-14 00:45:35 +0530

    sub

--- server.js
+++ server.js
@@ -16,9 +16,15 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/',analysis);
 
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`)
 })
